967bb1fe0da57756a4f24a4e8486be06246d7ef2
Chandra Prakash Singh <cprakashsingh2306@gmail.com>
2024-05-24 20:19:57 +0530

    Initial Commit

---
+++ mysite/view.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    fname = request.GET.get('fname')
+    lname = request.GET.get('lname')
+    params = {'fname':fname,'lname':lname}
+    return render(request,'index.html',params)
+
+def login(request):
+    return render(request,'login.html')
