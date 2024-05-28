ca9479923063d500a57141326b1bbc12bf2274bd
Chandra Prakash Singh <cprakashsingh2306@gmail.com>
2024-05-28 15:14:57 +0530

    working on authentication

--- mysite/view.py
+++ mysite/view.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
+from .models import User
+from .forms import MyUserCreationForm
+
+
 def index(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
@@ -7,7 +13,38 @@ def index(request):
     return render(request,'index.html',params)
 
 def login(request):
+    # page = 'login'
+    # if request.user.is_authenticated: 
+    #     return redirect('index')
+    # if request.method == 'POST':
+    #     email = request.POST.get('email').lower()
+    #     password = request.POST.get('password')
+
+    #     try:
+    #         user = User.objects.get(email=email)
+    #     except:
+    #         messages.error(request,'User does not Exits')
+
+    #     user = authenticate(request,email=email,password=password)
+
+    #     if user is not None:
+    #         login(request,user)
+    #         return redirect('index')
+    #     else:
+    #         messages.error(request,'Username or password does not matched')  
+    # context={'page':page}
     return render(request,'login.html')
 
 def register(request):
-    return render(request,'register.html')
+    form = MyUserCreationForm()
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.error(request,'An error occured during registration')
+    return render(request,'register.html',{'form':form})
